12288041c9807c212b230b69645bef2a5bc8725a
omer <justletomer@gmail.com>
2025-01-20 14:32:25 +0900

    제품 상세

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import "./App.css";
-import HomePage from "./components/Home/HomePage";
 import Navbar from "./components/Navbar/Navbar";
 import ProductsPage from "./components/products/ProductsPage";
+import SingleProductPage from "./components/SingleProduct/SingleProductPage";
 
 function App() {
   return (
     <div className="app">
       <Navbar />
-      <ProductsPage />
+      {/* <ProductsPage /> */}
+      <SingleProductPage />
     </div>
   );
 }
