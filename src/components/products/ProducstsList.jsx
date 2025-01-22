a547c08a530c6eeae519e112624353acc637731a
omer <justletomer@gmail.com>
2025-01-22 10:19:29 +0900

    isLoading추가

--- src/components/products/ProducstsList.jsx
+++ src/components/products/ProducstsList.jsx
@@ -4,7 +4,7 @@ import useData from "../../assets/Hook/useData";
 import ProductCardSkeleton from "./ProductCardSkeleton";
 
 const ProducstsList = () => {
-  const { data, error } = useData("/products");
+  const { data, error, isLoading } = useData("/products");
   const skeletons = [1, 2, 3, 4, 5, 6, 7, 8];
   return (
     <section className="products_list_section">
@@ -21,9 +21,7 @@ const ProducstsList = () => {
 
       <div className="products_list">
         {error && <em className="form_error">{error}</em>}
-        {skeletons.map((n) => (
-          <ProductCardSkeleton key={n} />
-        ))}
+        {isLoading && skeletons.map((n) => <ProductCardSkeleton key={n} />)}
         {data.products &&
           data.products.map((p) => (
             <ProductCard
