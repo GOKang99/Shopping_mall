f1e8510eb9cd8dc08a3c2c5e8dff4e9ff2433bd5
omer <justletomer@gmail.com>
2025-01-23 15:28:46 +0900

    Db에 장바구니 저장

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Navbar from "./components/Navbar/Navbar";
 import Routing from "./components/Routing/Routing";
 import { jwtDecode } from "jwt-decode";
 import setAuthToken from "./utils/setAuthToken";
+import { addToCartAPI } from "./components/Services/cartServices";
 //만약 토큰이 있으면 axios 설정에 추가됨
 setAuthToken(localStorage.getItem("token"));
 function App() {
@@ -21,6 +22,10 @@ function App() {
       alert("이미 추가 된 항목입니다,");
     }
     setCart(UpdatedCart);
+
+    addToCartAPI(product._id, quantity)
+      .then((res) => console.log(res.data))
+      .catch((err) => console.log(err.response));
   };
   useEffect(() => {
     try {
