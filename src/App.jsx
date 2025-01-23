2079effd67d995a44b76012fd724bae19b8a48f0
omer <justletomer@gmail.com>
2025-01-23 13:55:06 +0900

    토큰 만료 후 로컬스토리지 삭제

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,24 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
 import Routing from "./components/Routing/Routing";
+import { jwtDecode } from "jwt-decode";
 
 function App() {
+  const [user, setUser] = useState(null);
+  useEffect(() => {
+    try {
+      const jwt = localStorage.getItem("token");
+      const jwtUser = jwtDecode(jwt);
+      if (Date.now() >= jwtUser.exp * 1000) {
+        localStorage.removeItem("token");
+        window.location.reload();
+      } else {
+        setUser(jwtUser);
+      }
+    } catch (error) {} //토큰이 없을경우 아무 처리 x
+  });
+
   return (
     <div className="app">
       <Navbar />
